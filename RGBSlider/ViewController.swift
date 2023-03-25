89ced806b0eb1c70b1459c251cdd4c051c8ff7e2
Slavk <slavk11@yandex.ru>
2023-03-25 09:57:46 +0300

    added functional for sliders

--- RGBSlider/ViewController.swift
+++ RGBSlider/ViewController.swift
@@ -7,13 +7,42 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+final class ViewController: UIViewController {
+    
+    @IBOutlet var rGBView: UIView!
+    
+    @IBOutlet var redSliderValue: UILabel!
+    @IBOutlet var greenSliderValue: UILabel!
+    @IBOutlet var blueSliderValue: UILabel!
+    
+    @IBOutlet private var redSlider: UISlider!
+    @IBOutlet private var greenSlider: UISlider!
+    @IBOutlet private var blueSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        rGBView.layer.cornerRadius = 10
     }
 
-
+    @IBAction private func redSliderChangedValue() {
+        let roundedValue = round(redSlider.value / 0.01) * 0.01
+        redSlider.value = roundedValue
+        redSliderValue.text = redSlider.value.formatted()
+    }
+    
+    @IBAction private func greenSliderChangedValue() {
+        let roundedValue = round(greenSlider.value / 0.01) * 0.01
+        greenSlider.value = roundedValue
+        greenSliderValue.text = greenSlider.value.formatted()
+    }
+    
+    @IBAction func blueSliderChangedValue() {
+        let roundedValue = round(blueSlider.value / 0.01) * 0.01
+        blueSlider.value = roundedValue
+        blueSliderValue.text = blueSlider.value.formatted()
+    }
+    
+    
 }
 
